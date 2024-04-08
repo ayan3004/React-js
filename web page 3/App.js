a88b9c88d97c5a21ef1565ce487769f196199f70
ayan3004 <ayansangani3004@gmail.com>
2024-04-08 22:17:22 +0530

    assignment3

---
+++ web page 3/App.js
@@ -0,0 +1,22 @@
+import logo from './logo.svg';
+import './App.css';
+import Navbar from './Navbar';
+import Section1 from './Section1';
+import Section2 from './Section2';
+import Section3 from './Section3';
+import Section4 from './Section4';
+import Footer from './Footer';
+function App() {
+  return (
+    <>
+    <Navbar />
+    <Section1 />
+    <Section2 />
+    <Section3 />
+    <Section4 />
+    <Footer />
+    </>
+  );
+}
+
+export default App;
