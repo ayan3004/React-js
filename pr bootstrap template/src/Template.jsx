4c019b76ab299b1f7af94eaa2c646347d88fe662
ayan3004 <ayansangani3004@gmail.com>
2024-05-01 18:13:49 +0530

    pr bootstrap

---
+++ pr bootstrap template/src/Template.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import Navbar from './Components/Navbar'
+import Header from './Components/Header'
+import Section1 from './Components/Section1'
+import Section2 from './Components/Section2'
+
+function Template() {
+  return (
+    <div>
+        <Navbar/>
+        <Header />
+        <Section1 />
+        <Section2 />
+    </div>
+  )
+}
+
+export default Template
